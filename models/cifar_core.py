c5d0b2660cb689e0471ca651b8bfa89083629697
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2023-04-28 19:34:34 -0400

    Add bias to cifar_core.py

--- models/cifar_core.py
+++ models/cifar_core.py
@@ -10,6 +10,7 @@ from models import basenet
 from models import dataloader
 import utils
 
+
 class CifarModel():
     def __init__(self, opt):
         super(CifarModel, self).__init__()
@@ -19,14 +20,14 @@ class CifarModel():
         self.print_freq = opt['print_freq']
         self.init_lr = opt['optimizer_setting']['lr']
         self.log_writer = SummaryWriter(os.path.join(self.save_path, 'logfile'))
-        
+
         self.set_network(opt)
         self.set_data(opt)
         self.set_optimizer(opt)
 
     def set_network(self, opt):
         """Define the network"""
-        
+
         self.network = basenet.ResNet18(num_classes=opt['output_dim']).to(self.device)
 
     def forward(self, x):
@@ -35,7 +36,7 @@ class CifarModel():
 
     def set_data(self, opt):
         """Set up the dataloaders"""
-        
+
         data_setting = opt['data_setting']
 
         with open(data_setting['train_data_path'], 'rb') as f:
@@ -63,46 +64,46 @@ class CifarModel():
             normalize,
         ])
 
-        train_data = dataloader.CifarDataset(data_setting['train_data_path'], 
+        train_data = dataloader.CifarDataset(data_setting['train_data_path'],
                                              data_setting['train_label_path'],
                                              transform_train)
-        test_color_data = dataloader.CifarDataset(data_setting['test_color_path'], 
+        test_color_data = dataloader.CifarDataset(data_setting['test_color_path'],
                                                   data_setting['test_label_path'],
                                                   transform_test)
-        test_gray_data = dataloader.CifarDataset(data_setting['test_gray_path'], 
+        test_gray_data = dataloader.CifarDataset(data_setting['test_gray_path'],
                                                  data_setting['test_label_path'],
                                                  transform_test)
 
         self.train_loader = torch.utils.data.DataLoader(
-                                 train_data, batch_size=opt['batch_size'],
-                                 shuffle=True, num_workers=1)
+            train_data, batch_size=opt['batch_size'],
+            shuffle=True, num_workers=0)
         self.test_color_loader = torch.utils.data.DataLoader(
-                                      test_color_data, batch_size=opt['batch_size'],
-                                      shuffle=False, num_workers=1)
+            test_color_data, batch_size=opt['batch_size'],
+            shuffle=False, num_workers=0)
         self.test_gray_loader = torch.utils.data.DataLoader(
-                                     test_gray_data, batch_size=opt['batch_size'],
-                                     shuffle=False, num_workers=1)
-    
+            test_gray_data, batch_size=opt['batch_size'],
+            shuffle=False, num_workers=0)
+
     def set_optimizer(self, opt):
         optimizer_setting = opt['optimizer_setting']
-        self.optimizer = optimizer_setting['optimizer']( 
-                            params=self.network.parameters(), 
-                            lr=optimizer_setting['lr'],
-                            momentum=optimizer_setting['momentum'],
-                            weight_decay=optimizer_setting['weight_decay']
-                            )
-        
+        self.optimizer = optimizer_setting['optimizer'](
+            params=self.network.parameters(),
+            lr=optimizer_setting['lr'],
+            momentum=optimizer_setting['momentum'],
+            weight_decay=optimizer_setting['weight_decay']
+        )
+
     def _criterion(self, output, target):
         return F.cross_entropy(output, target)
-        
+
     def state_dict(self):
         state_dict = {
             'model': self.network.state_dict(),
             'optimizer': self.optimizer.state_dict(),
             'epoch': self.epoch
-        }  
+        }
         return state_dict
-    
+
     def load_state_dict(self, state_dict):
         self.network.load_state_dict(state_dict['model'])
         self.optimizer.load_state_dict(state_dict['optimizer'])
@@ -118,10 +119,10 @@ class CifarModel():
 
     def _train(self, loader):
         """Train the model for one epoch"""
-        
+
         self.network.train()
         self.adjust_lr()
-        
+
         train_loss = 0
         total = 0
         correct = 0
@@ -137,18 +138,18 @@ class CifarModel():
             _, predicted = outputs.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
-            accuracy = correct*100. / total
+            accuracy = correct * 100. / total
 
             train_result = {
-                'accuracy': correct*100. / total,
+                'accuracy': correct * 100. / total,
                 'loss': loss.item(),
             }
             self.log_result('Train iteration', train_result,
-                            len(loader)*self.epoch + i)
+                            len(loader) * self.epoch + i)
 
             if self.print_freq and (i % self.print_freq == 0):
                 print('Training epoch {}: [{}|{}], loss:{}, accuracy:{}'.format(
-                    self.epoch, i+1, len(loader), loss.item(), accuracy
+                    self.epoch, i + 1, len(loader), loss.item(), accuracy
                 ))
 
         self._train_accuracy = accuracy
@@ -156,7 +157,7 @@ class CifarModel():
 
     def _test(self, loader):
         """Test the model performance"""
-        
+
         self.network.eval()
 
         total = 0
@@ -181,12 +182,49 @@ class CifarModel():
                 feature_list.append(features.cpu().numpy())
 
         test_result = {
-            'accuracy': correct*100. / total,
+            'accuracy': correct * 100. / total,
             'predict_labels': predict_list,
             'outputs': np.vstack(output_list),
             'features': np.vstack(feature_list)
         }
         return test_result
+
+    def _final_test(self, loader):
+        """Test the model bias"""
+
+        self.network.eval()
+
+        total = 0
+        correct = 0
+        test_loss = 0
+        output_list = []
+        feature_list = []
+        predict_list = []
+        class_count = [0] * 20
+        with torch.no_grad():
+            for i, (images, targets) in enumerate(loader):
+                images, targets = images.to(self.device), targets.to(self.device)
+                outputs, features = self.forward(images)
+                loss = self._criterion(outputs, targets)
+                test_loss += loss.item()
+
+                _, predicted = outputs.max(1)
+                total += targets.size(0)
+                correct += predicted.eq(targets).sum().item()
+                for predict in predicted:
+                    class_count[predict] += 1
+
+                predict_list.extend(predicted.tolist())
+                output_list.append(outputs.cpu().numpy())
+                feature_list.append(features.cpu().numpy())
+
+        test_result = {
+            'accuracy': correct * 100. / total,
+            'predict_labels': predict_list,
+            'outputs': np.vstack(output_list),
+            'features': np.vstack(feature_list)
+        }
+        return test_result, class_count
 
     def train(self):
         self._train(self.train_loader)
@@ -194,18 +232,28 @@ class CifarModel():
 
     def test(self):
         # Test and save the result
-        test_color_result = self._test(self.test_color_loader)
-        test_gray_result = self._test(self.test_gray_loader)
+        # test_color_result = self._test(self.test_color_loader)
+        # test_gray_result = self._test(self.test_gray_loader)
+        test_color_result, color_class_count = self._final_test(self.test_color_loader)
+        test_gray_result, gray_class_count = self._final_test(self.test_gray_loader)
+        bias_sum = 0
+
+        # Calculate bias
+        for i in range(10):
+            color_class_count[i] = color_class_count[i] + color_class_count[
+                i + 10]  # combine the smae class of different domains
+            gray_class_count[i] = gray_class_count[i] + gray_class_count[i + 10]
+            color_class = color_class_count[i]
+            gray_class = gray_class_count[i]
+            bias_sum += max(color_class, gray_class) / (color_class + gray_class)
+        bias = bias_sum / 10 - 0.5
+
         utils.save_pkl(test_color_result, os.path.join(self.save_path, 'test_color_result.pkl'))
         utils.save_pkl(test_gray_result, os.path.join(self.save_path, 'test_gray_result.pkl'))
-        
+
         # Output the classification accuracy on test set
-        info = ('Test on color images accuracy: {}\n' 
-                'Test on gray images accuracy: {}'.format(test_color_result['accuracy'],
-                                                          test_gray_result['accuracy']))
+        info = ('Bias: {}\n'
+                'Test on color images accuracy: {}\n'
+                'Test on gray images accuracy: {}\n'.format(bias, test_color_result['accuracy'],
+                                                            test_gray_result['accuracy']))
         utils.write_info(os.path.join(self.save_path, 'test_result.txt'), info)
-        
-    
-
-
-            
